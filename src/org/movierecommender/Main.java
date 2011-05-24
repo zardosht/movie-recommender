f4294a588b1054ba15385cc5a593726e6cff29fd
Otto <wesendon@in.tum.de>
2011-05-24 15:42:04 +0200

    added csv writer
    Change-Id: I5402b65c59f9ff417ead6ed6cf62ebe082b60840

--- src/org/movierecommender/Main.java
+++ src/org/movierecommender/Main.java
@@ -2,8 +2,11 @@ package org.movierecommender;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
+import org.movierecommender.data.CSVWriter;
 import org.movierecommender.data.ImportUtil;
 import org.movierecommender.driver.Driver;
 import org.movierecommender.driver.prediction.MeanPredictor;
@@ -14,7 +17,16 @@ import org.movierecommender.model.UserItemMatrix;
 public class Main {
 	// test //test //test //test
 	public static void main(String[] args) throws IOException {
-			
+		
+//		CSVWriter writer = new CSVWriter(new File("C:/Users/Otto/Desktop/test.csv"), Arrays.asList("Z","O"));
+//		HashMap<String,Object> map = new HashMap<String, Object>();
+//		map.put("Z", "31");
+//		map.put("O", "24");
+//		writer.writeRecord(map);
+//		writer.close();
+//		
+//		System.exit(0);
+		
 		UserItemMatrix matrix = ImportUtil.importUserItemFromFile(new File(
 				"data/ml-data_0/u.data"));
 		Driver driver = new Driver(matrix, null, null);
