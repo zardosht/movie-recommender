bbcc126b978dcfdf9897672f8bf36a3d25204fa1
zardosht <zardosht@dorosht>
2011-05-29 00:36:22 +0200

    configuration for production
    Change-Id: Ie6d709ee6c5dabc2c1b48154d81dfc0bdbdf43c7

--- src/org/movierecommender/data/Configuration.java
+++ src/org/movierecommender/data/Configuration.java
@@ -3,29 +3,57 @@ package org.movierecommender.data;
 import java.util.List;
 import java.util.Properties;
 
+import org.movierecommender.controller.prediction.MeanPredictor;
 import org.movierecommender.controller.prediction.RatingPredictor;
+import org.movierecommender.controller.prediction.WeightedPredictor;
+import org.movierecommender.controller.similarity.MeanSquaredErrorStrategy;
+import org.movierecommender.controller.similarity.PearsonCorrelationStrategy;
 import org.movierecommender.controller.similarity.SimilarityStrategy;
 
 public class Configuration extends Properties {
 	
-	List<SimilarityStrategy> simStats;
-	int kNStart;
-	int kNEnd;
-	int kNStep;
-	List<RatingPredictor> predStats;
-	double favThresholdStart;
-	double favThresholdEnd;
-	double favThresholdStep;
+	public String getMRMode(){
+		return getProperty("mr.mode");
+	}
 	
+	public SimilarityStrategy getProductionSimilarityStrategy(){
+		String property = getProperty("mr.production.simstrat");
+		if("mse".equals(property)){
+			return new MeanSquaredErrorStrategy();
+		}else if("pearson".equals(property)){
+			return new PearsonCorrelationStrategy();
+		}
+		return null;
+	}
 
+	public int getProductionKNeighbors(){
+		return Integer.parseInt(getProperty("mr.production.kNeighbors"));
+	}
+
+	
+	public RatingPredictor getProductionPredictionStrategy(){
+		String property = getProperty("mr.production.predStrat");
+		if("mean".equals(property)){
+			return new MeanPredictor();
+		}else if("weighted".equals(property)){
+			return new WeightedPredictor();
+		}
+		return null;
+	}
+	
+	public double getProductionFavoriteThreshold(){
+		return Double.parseDouble(getProperty("mr.production.favThreshold"));
+	}
+
+
+	
 	public List<SimilarityStrategy> getSimilarityStrats() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	public int getKNStart() {
-		// TODO Auto-generated method stub
-		return 0;
+		return Integer.parseInt(getProperty("mr.evalulation.kstart"));
 	}
 
 	public int getKNEnd() {
